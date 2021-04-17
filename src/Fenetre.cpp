5b73195d8d638f87bf6e4fc1cac3ac275b08de87
LALERE FRANKLIN p1802899 <franklin.lalere@etu.univ-lyon1.fr>
2021-04-17 13:50:28 +0200

    Création de gtkmm-template

---
+++ src/Fenetre.cpp
@@ -0,0 +1,8 @@
+#include "Fenetre.hpp"
+
+Fenetre::Fenetre()
+{
+    set_default_size(400, 400);
+
+    show_all();
+}
---
+++ src/Fenetre.hpp
@@ -0,0 +1,17 @@
+#ifndef DEF_FENETRE
+#define DEF_FENETRE
+
+#include <gtkmm/main.h>
+#include <gtkmm/window.h>
+#include <string>
+#include <iostream>
+
+class Fenetre : public Gtk::Window
+{
+    public:
+        Fenetre();
+
+
+};
+
+#endif
